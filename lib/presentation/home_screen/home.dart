3fa89f3d692adef937ab26e1d8d97b2b3c57f3c6
Mr-Ashitey <aamarh17@gmail.com>
2023-04-17 14:20:24 +0000

    add list item card

--- lib/presentation/home_screen/home.dart
+++ lib/presentation/home_screen/home.dart
@@ -6,8 +6,39 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Grocery Lists"),
+      appBar: AppBar(title: const Text("My Grocery Lists")),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          ListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            tileColor: Colors.black12,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            title: const Text(
+              "Item 1",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+            ),
+            subtitle: Row(
+              children: const [
+                SizedBox(
+                  width: 150,
+                  child: LinearProgressIndicator(
+                    value: 0.5,
+                    minHeight: 5,
+                  ),
+                ),
+                SizedBox(width: 8),
+                Text(
+                  "2 / 2",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            trailing: const Icon(Icons.more_vert_rounded),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
