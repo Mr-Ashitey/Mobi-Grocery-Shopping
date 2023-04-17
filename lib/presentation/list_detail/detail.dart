d40128133954f7109a6b210acda0334394a980f1
Mr-Ashitey <aamarh17@gmail.com>
2023-04-17 16:15:42 +0000

    add body section of detail screen

--- lib/presentation/list_detail/detail.dart
+++ lib/presentation/list_detail/detail.dart
@@ -10,6 +10,38 @@ class ListDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text(listTitle),
       ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: const LinearProgressIndicator(
+              value: 0.5,
+              minHeight: 10,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 8,
+              itemBuilder: (_, itemCount) {
+                return CheckboxListTile(
+                  onChanged: (changedValue) {},
+                  value: true,
+                  activeColor: Colors.green,
+                  title: const Text(
+                    "Bread",
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  secondary: const Icon(Icons.local_grocery_store_rounded),
+                  checkboxShape: const CircleBorder(),
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(),
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
         icon: const Icon(Icons.add),
