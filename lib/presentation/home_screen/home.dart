3102ae05e3f267e2050611a48764a88f5817208d
Mr-Ashitey <aamarh17@gmail.com>
2023-04-18 14:09:10 +0000

    use dummy grocery list data

--- lib/presentation/home_screen/home.dart
+++ lib/presentation/home_screen/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobi_grocery_shopping/core/helper_functions.dart';
+import 'package:mobi_grocery_shopping/core/model/grocery_model.dart';
 import 'package:mobi_grocery_shopping/core/utils/alert.dart';
 import 'package:mobi_grocery_shopping/core/utils/bottom_modal.dart';
 
@@ -32,8 +34,11 @@ class _HomeState extends State<Home> {
       appBar: AppBar(title: const Text("My Grocery Lists")),
       body: ListView.separated(
         padding: const EdgeInsets.all(10),
-        itemCount: 10,
+        itemCount: groceryList.length,
         itemBuilder: (_, itemCount) {
+          final groceryItems = groceryList[itemCount].items;
+          final collectedGroceryItems =
+              getCollectedGroceryItems(groceryItems ?? []);
           return ListTile(
             tileColor: Colors.black12,
             contentPadding:
@@ -41,34 +46,37 @@ class _HomeState extends State<Home> {
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             title: Text(
-              "Item $itemCount",
+              groceryList[itemCount].name ?? "",
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
             ),
             subtitle: Row(
-              children: const [
+              children: [
                 SizedBox(
                   width: 150,
                   child: LinearProgressIndicator(
-                    value: 0.5,
+                    value: collectedGroceryItems.length / groceryItems!.length,
                     minHeight: 5,
                   ),
                 ),
-                SizedBox(width: 8),
+                const SizedBox(width: 8),
                 Text(
-                  "2 / 2",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  "${collectedGroceryItems.length} / ${groceryItems.length}",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
               ],
             ),
             trailing: IconButton(
               icon: const Icon(Icons.more_vert_rounded),
               onPressed: () {
-                context.showItemOptions(itemName: "Item $itemCount");
+                context.showItemOptions(
+                    groceryId: groceryList[itemCount].id ?? 0,
+                    itemName: groceryList[itemCount].name ?? "");
               },
             ),
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(
-                  builder: (_) => ListDetail(listTitle: "Item $itemCount")));
+                  builder: (_) => ListDetail(
+                      listTitle: groceryList[itemCount].name ?? "")));
             },
           );
         },
