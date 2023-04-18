eaa6ac15a49aac71fae109a337e4ad12d1e89baa
Mr-Ashitey <aamarh17@gmail.com>
2023-04-18 11:33:50 +0000

    add slidable package to edit and delete items

--- lib/presentation/list_detail/detail.dart
+++ lib/presentation/list_detail/detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:mobi_grocery_shopping/core/utils/add_item_util.dart';
 
 class ListDetail extends StatelessWidget {
@@ -26,16 +27,50 @@ class ListDetail extends StatelessWidget {
             child: ListView.separated(
               itemCount: 8,
               itemBuilder: (_, itemCount) {
-                return CheckboxListTile(
-                  onChanged: (changedValue) {},
-                  value: true,
-                  activeColor: Colors.green,
-                  title: const Text(
-                    "Bread",
-                    style: TextStyle(fontWeight: FontWeight.w700),
+                return Slidable(
+                  // Specify a key if the Slidable is dismissible.
+                  key: const ValueKey(0),
+
+                  // The start action pane is the one at the left or the top side.
+                  endActionPane: ActionPane(
+                    // A motion is a widget used to control how the pane animates.
+                    motion: const BehindMotion(),
+
+                    // A pane can dismiss the Slidable.
+                    // dismissible: DismissiblePane(onDismissed: () {}),
+
+                    // All actions are defined in the children parameter.
+                    children: [
+                      // A SlidableAction can have an icon and/or a label.
+                      SlidableAction(
+                        onPressed: (_) {
+                          context.showItemDialog(isEdit: true);
+                        },
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                        icon: Icons.edit,
+                        label: 'Edit',
+                      ),
+                      SlidableAction(
+                        onPressed: (_) {},
+                        backgroundColor: const Color(0xFFFE4A49),
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
+                      ),
+                    ],
                   ),
-                  secondary: const Icon(Icons.local_grocery_store_rounded),
-                  checkboxShape: const CircleBorder(),
+                  child: CheckboxListTile(
+                    onChanged: (changedValue) {},
+                    value: true,
+                    activeColor: Colors.green,
+                    title: const Text(
+                      "Bread",
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                    secondary: const Icon(Icons.local_grocery_store_rounded),
+                    checkboxShape: const CircleBorder(),
+                  ),
                 );
               },
               separatorBuilder: (context, index) => const Divider(),
@@ -45,7 +80,7 @@ class ListDetail extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          context.showAddItemDialog();
+          context.showItemDialog();
         },
         icon: const Icon(Icons.add),
         label: const Text("ADD"),
