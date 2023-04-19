22641065eb50266b1e1416d1257eb66d28441bf9
Mr-Ashitey <aamarh17@gmail.com>
2023-04-19 12:01:59 +0000

    add grocery list manager provider to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mobi_grocery_shopping/core/viewModels/grocery_manager.dart';
+import 'package:provider/provider.dart';
 
 import 'presentation/splash_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      theme: ThemeData(
-          fontFamily: "SF Pro",
-          appBarTheme: const AppBarTheme(color: Colors.black),
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            backgroundColor: Colors.black,
-            extendedTextStyle: TextStyle(fontWeight: FontWeight.w600),
-          )),
-      home: const SplashScreen(),
+    ChangeNotifierProvider.value(
+      value: GroceryListManager(),
+      child: MaterialApp(
+        theme: ThemeData(
+            fontFamily: "SF Pro",
+            appBarTheme: const AppBarTheme(color: Colors.black),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.black,
+              extendedTextStyle: TextStyle(fontWeight: FontWeight.w600),
+            )),
+        home: const SplashScreen(),
+      ),
     ),
   );
 }
