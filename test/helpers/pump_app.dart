1ca6c387cc1ed569962814342bf43b6ba0274fcc
Mr-Ashitey <aamarh17@gmail.com>
2023-04-19 17:54:06 +0000

    make pump app extension reusable

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobi_grocery_shopping/core/viewModels/grocery_manager.dart';
-import 'package:mobi_grocery_shopping/presentation/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(String route) {
+  Future<void> pumpApp(Widget screen) {
     return pumpWidget(
       ChangeNotifierProvider.value(
         value: GroceryListManager(),
-        child: const MaterialApp(
-          home: SplashScreen(),
+        child: MaterialApp(
+          home: screen,
         ),
       ),
     );
